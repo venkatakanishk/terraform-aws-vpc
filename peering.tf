1d095964a9319e2b41a8fb3ad44d92c05bccad74
Venkata Kanishk Payyavala <venkatakanishk244@gmail.com>
2026-03-04 20:08:25 +0530

    changed something

--- peering.tf
+++ peering.tf
@@ -22,4 +22,28 @@ tags = merge(local.common_tags,
                                         })
 }
 
+resource "aws_route" "private_peering" {
+  count = var.is_peering_required ? 1 : 0
+  route_table_id            = aws_route_table.private.id
+  destination_cidr_block    = data.aws_vpc.default.cidr_block # this is routing to route main vpc through destination default cidr value  through peering
+  vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
+}
+resource "aws_route" "database_peering" {
+  count = var.is_peering_required ? 1 : 0
+  route_table_id            = aws_route_table.database.id
+  destination_cidr_block    = data.aws_vpc.default.cidr_block # this is routing to route main vpc through destination default cidr value  through peering
+  vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
+}
+resource "aws_route" "public_peering" {
+    count = var.is_peering_required ? 1 : 0
+    route_table_id            = aws_route_table.public.id
+    destination_cidr_block    = data.aws_vpc.default.cidr_block # this is routing to route main vpc through destination default cidr value  through peering
+    vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
+}
 
+resource "aws_route" "default_peering" {
+    count = var.is_peering_required ? 1 : 0
+    route_table_id            = data.aws_route_table.default.id
+    destination_cidr_block    = var.vpc_cidr # this is routing to route vpc cidr in default vpc default route table value  through peering
+    vpc_peering_connection_id = aws_vpc_peering_connection.default[count.index].id
+}
