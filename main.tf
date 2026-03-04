ca6cdc4af790619d331335ec75d3765f2db9061a
Venkata Kanishk Payyavala <venkatakanishk244@gmail.com>
2026-03-04 12:09:32 +0530

    changed something

--- main.tf
+++ main.tf
@@ -123,3 +123,20 @@ resource "aws_route" "database" {
   destination_cidr_block    = "0.0.0.0/0" # this is routing to route 0.0.0.0/0 through nat gateway
   nat_gateway_id = aws_nat_gateway.this.id
 }
+
+resource "aws_route_table_association" "public" {
+  count = length(var.public_subnet_cidr)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+resource "aws_route_table_association" "private" {
+  count = length(var.private_subnet_cidr)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "database" {
+  count = length(var.database_subnet_cidr)
+  subnet_id      = aws_subnet.database[count.index].id
+  route_table_id = aws_route_table.database.id
+}
