61426a9ee66923acf2b58f5ebf4492a9a255d4a5
Venkata Kanishk Payyavala <venkatakanishk244@gmail.com>
2026-03-06 15:34:06 +0530

    changed something

--- outputs.tf
+++ outputs.tf
@@ -6,5 +6,10 @@ output "vpc_id" {
 }
 output "public_subnet_ids" {
     value = aws_subnet.public[*].id
-  
+}
+output "private_subnet_ids" {
+    value = aws_subnet.private[*].id
+}
+output "database_subnet_ids" {
+    value = aws_subnet.database[*].id
 }
